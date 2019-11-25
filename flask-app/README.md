c0696bfe538f46157a1aa6c9cefeb3f996d6908a
Norbert Barto <norbert.barto@t-systems.com>
2019-11-25 16:20:25 +0100

    flask app

---
+++ flask-app/README.md
@@ -0,0 +1,7 @@
+## Setting up the python3 environment
+```
+sudo yum list | grep python3
+sudo yum install python3 -y
+pip3 install flask
+python index.py
+```
---
+++ flask-app/hello.py
@@ -0,0 +1,13 @@
+# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    # is port 80 opened in the security group?
+    app.run(host='0.0.0.0', port=80)
+
